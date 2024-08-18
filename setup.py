31aa1dae26727bb20a09562754abb10da7547e2d
Gabriel Porteiro <71458971+GportDev@users.noreply.github.com>
2024-08-17 19:51:24 -0500

    Bump version to 0.2.3 - Fix relative path for version.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import re
 from setuptools import setup, find_packages
 from typing import List
 
-with io.open("docs/version.py", "rt", encoding="utf8") as f:
+with io.open("./docs/version.py", "rt", encoding="utf8") as f:
     version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
 
 with io.open("README.rst", "rt", encoding="utf8") as f:
