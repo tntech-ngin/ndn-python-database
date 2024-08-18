bdafecd9973ba5d291e4e5c4958abc9909fd008b
Gabriel Porteiro <71458971+GportDev@users.noreply.github.com>
2024-08-17 19:45:07 -0500

    Bump version to 0.2.3 - Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
-#    @Author: Justin C Presley
-#    @Author-Email: justincpresley@gmail.com
-#    @Project: NDN Storage
-#    @Source-Code: https://github.com/justincpresley/ndn-python-storage
-#    @Pip-Library: https://pypi.org/project/ndn-storage
+#  @Project: NDN Storage
+#  @Authors: Please check AUTHORS.rst
+#  @Source-Code: https://github.com/tntech-ngin/ndn-python-database
+#  @Pip-Library: https://pypi.org/project/ndn-python-database
 
-#!/usr/bin/env python3
 import io
 import re
 from setuptools import setup, find_packages
@@ -16,25 +14,27 @@ with io.open("docs/version.py", "rt", encoding="utf8") as f:
 with io.open("README.rst", "rt", encoding="utf8") as f:
     long_description = f.read()
 
+
 def _parse_requirements(filename: str) -> List[str]:
     with open(filename, 'r') as f:
-        return [s for s in [line.split('#', 1)[0].strip(' \t\n') for line in f ] if s != '']
+        return [s for s in [line.split('#', 1)[0].strip(' \t\n') for line in f] if s != '']
+
 
 setup(
-    name='ndn-storage',
+    name='ndn-python-database',
     version=version,
     description='An NDN Storage Library for Applications in Python 3',
     long_description=long_description,
     long_description_content_type='text/x-rst',
-    url='https://github.com/justincpresley/ndn-python-storage',
-    author='Justin C Presley',
-    author_email='justincpresley@gmail.com',
-    maintainer='Justin C Presley',
-    maintainer_email='justincpresley@gmail.com',
-    download_url='https://pypi.python.org/pypi/ndn-storage',
+    url='https://github.com/tntech-ngin/ndn-python-database',
+    author='See GitHub',
+    author_email='sshannigrahi@tntech.edu',
+    maintainer='see GitHub',
+    maintainer_email='sshannigrahi@tntech.edu',
+    download_url='https://pypi.python.org/pypi/ndn-python-database',
     project_urls={
-        "Bug Tracker": "https://github.com/justincpresley/ndn-python-storage/issues",
-        "Source Code": "https://github.com/justincpresley/ndn-python-storage",
+        "Bug Tracker": "https://github.com/tntech-ngin/ndn-python-database/issues",
+        "Source Code": "https://github.com/tntech-ngin/ndn-python-database",
     },
     license='Apache License 2.0',
     classifiers=[
@@ -51,6 +51,8 @@ setup(
     keywords='NDN STORAGE DISK MEMORY CACHE',
     packages=find_packages('src'),
     package_dir={'': 'src'},
+    include_package_data=True,
+    package_data={'ndn-python-database': ['config.yaml', 'docs/version.py', 'docs/requirements.txt']},
     install_requires=_parse_requirements('docs/requirements.txt'),
     python_requires=">=3.8",
     zip_safe=False)
